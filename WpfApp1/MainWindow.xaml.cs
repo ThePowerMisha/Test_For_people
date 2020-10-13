8ea57a86419490c0c2488d96e78218ce59d1c65c
Alex <69731857+AlexanderTheGod@users.noreply.github.com>
2020-10-13 22:24:37 +0400

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using WpfApp1.View;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window{
+        public MainWindow() {
+            InitializeComponent();
+
+            //Настройка размеров всего приложения
+            this.WindowState = WindowState.Maximized;
+            this.WindowStyle = WindowStyle.None;
+
+            //ояистка шлавного грида
+            //mainContentGrid.Children.RemoveRange(0, mainContentGrid.Children.Count);
+        }
+
+        //=======================mainLayout=======================
+
+        //-----LoadedEvent-----
+        private void windowApp_Loaded(object sender, RoutedEventArgs e) {
+            contentControl.Content = new mainPage(this, currentRecord);
+        }
+        private void navBarButton3_Click(object sender, RoutedEventArgs e) {
+            contentControl.Content = new aboutProgramm(contentControl);
+        }
+        private void navBarButton2_Click(object sender, RoutedEventArgs e) {
+            contentControl.Content = new spravkaPage(contentControl);
+        }
+        //-----LoadedEvent-----
+
+        //-----ClickEvent-----
+        private void mainLayoutExit_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+        //-----ClickEvent-----
+
+        //=======================mainLayout=======================
+    }
+}
---
+++ WpfApp1/SpecialColor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp1{
+    public class SpecialColor{
+        //fields
+        private static Color Transparent = Color.FromArgb(0, 0, 0, 0);
+        private static Color MainBack = Color.FromRgb(239, 239, 239);
+        private static Color MainBackHover = Color.FromRgb(228, 228, 228);
+        private static Color MainBlue = Color.FromRgb(62, 148, 209);
+        private static Color Red = Color.FromRgb(255, 64, 64);
+        private static Color Green = Color.FromRgb(54, 214, 149);
+        private static Color White = Color.FromRgb(255, 255, 255);
+        private static Color Black = Color.FromRgb(0, 0, 0);
+        private static Color Placeholder = Color.FromArgb(100, 0, 0, 0);
+
+        //getters
+        //return color from format 'Color.FromRgb'
+        public static SolidColorBrush getColor(Color color){
+            return new SolidColorBrush(color);
+        }
+        //return Transparent
+        public static SolidColorBrush transparent(){
+            return new SolidColorBrush(Transparent);
+        }
+        //return MainBack
+        public static SolidColorBrush mainBack(){
+            return new SolidColorBrush(MainBack);
+        }
+        //return MainBackHover
+        public static SolidColorBrush mainBackHover(){
+            return new SolidColorBrush(MainBackHover);
+        }
+        //return MainBlue
+        public static SolidColorBrush mainBlue(){
+            return new SolidColorBrush(MainBlue);
+        }
+        //return Red
+        public static SolidColorBrush red(){
+            return new SolidColorBrush(Red);
+        }
+        //return Green
+        public static SolidColorBrush green(){
+            return new SolidColorBrush(Green);
+        }
+        //return White
+        public static SolidColorBrush white(){
+            return new SolidColorBrush(White);
+        }
+        //return Black
+        public static SolidColorBrush black(){
+            return new SolidColorBrush(Black);
+        }
+        //return Placeholder
+        public static SolidColorBrush placeholder(){
+            return new SolidColorBrush(Placeholder);
+        }
+    }
+}
