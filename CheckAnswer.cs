b5c219b7195bf035bd5e91a712825c10b160f1a4
iokarch <71277386+iokarch@users.noreply.github.com>
2020-11-24 16:10:03 +0400

    Update CheckAnswer.cs
    Added a function to remove unnecessary spaces from a string. This is necessary so that the formula can be entered with spaces between characters.

--- CheckAnswer.cs
+++ CheckAnswer.cs
@@ -89,10 +89,9 @@ namespace TestTrainingProgram
                 {"h", 10}
             };
 
-            // Перевод строки в нижний регистр
-            Formula = Formula.ToLower();
+            // Удаляем ненужные пробелы из строки и переводим все в нижний регистр
+            Formula = Formula.Replace(" ", "").ToLower();
             
-
             // Делим введенную формулу на элементы массива, используя паттерн арифметический операций
             string[] array = Regex.Split(Formula, pattern);
 
