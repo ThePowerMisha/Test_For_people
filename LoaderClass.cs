741a43c5ada5c9d460304b5f6e5092e345ca6b84
ThePowerMisha <ThePowerMisha@users.noreply.github.com>
2020-12-01 20:46:46 +0400

    Add load params to LoaderClass.cs

--- LoaderClass.cs
+++ LoaderClass.cs
@@ -2,35 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Threading.Tasks;
 
 namespace WpfApp1
 {
-    public class LoaderClass
+   public class LoaderClass
     {
+        //Путь к вопросу
         private string path;
-        private Dictionary<string, int> dictionary;
+        
+        //Спиток известных переменных и их значений
+        private Dictionary<string, double> dictionary;
+        
+        //Текст вопроса
         private string QuestionText;
+        
+        //ОТветы в виде формул на вопрос
         private List<string> Formula;
-
+        
+        //Переменные которые нажо найти
+        private List<string> QuestionFindParams;
+        
+        //Конструктор класса
         public LoaderClass(string path)
         {
             this.path = path;
+            this.LoadQuestion();
         }
-
-        async void LoadQuestion()
+            
+        /// <summary>
+        /// Загрузка вопроса
+        /// </summary>
+        public void LoadQuestion()
         {
+            //Спиток известных переменных и их значений
             List<string> arr = new List<string>();
+            //ОТветы в виде формул на вопрос
             List<string> formula = new List<string>();
+            //Текст вопроса
             string text = "";
+            // Переменыне которые надо найти
+            List<string> Params = new List<string>();
             bool isDict = false;
             bool isText = false;
             bool isFormula = false;
+            bool isFindParams = false;
            
             using (StreamReader sr = new StreamReader(path))
             {
                 string line;
-                while ((line = await sr.ReadLineAsync()) != null)
+                while ((line =  sr.ReadLine()) != null)
                 {
 
                     if ("[/QuestionParams]" == line)
@@ -66,43 +88,81 @@ namespace WpfApp1
 
                     if ("[/QuestionText]" == line)
                     {
-                        isFormula = false;
+                        isText = false;
                     }
 
-                    if (isFormula)
+                    if (isText)
                     {
                         text = line;
                     }
 
                     if ("[QuestionText]" == line)
                     {
-                        isFormula = true;
+                        isText = true;
                     }
 
+                    if ("[/QuestionFind]" == line)
+                    {
+
+                        isFindParams = false;
+                    }
+
+                    if (isFindParams)
+                    {
+                        Params.Add(line);
+                    }
+
+                    if ("[QuestionFind]" == line)
+                    {
+                        isFindParams = true;
+                    }
                 }
             }
-
+            
+            //Преобразует текст в файле из string в словарь <string,double>
             var dict = arr.Select((s) => new {s})
-                .ToDictionary(x => x.s.Split(':')[0], x => int.Parse(x.s.Split(':')[1]));
+                .ToDictionary(x => x.s.Split(':')[0], x => double.Parse(x.s.Split(':')[1]));
             dictionary = dict;
             QuestionText = text;
             Formula = formula;
-
+            QuestionFindParams = Params;
         }
-
+        
+        /// <summary>
+        /// Возвращает текст вопроса
+        /// </summary>
+        /// <returns></returns>
         public string returnQuestionText()
         {
             return QuestionText;
         }
-
-        public Dictionary<string, int> returnVariables()
+        
+        
+        /// <summary>
+        /// Возвращает переменные и их значения
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, double> returnVariables()
         {
             return dictionary;
         }
-
+        
+        /// <summary>
+        /// Возвращает формулу вопроса
+        /// </summary>
+        /// <returns></returns>
         public List<string> returnFormula()
         {
             return Formula;
+        }
+
+        /// <summary>
+        /// Возвращает переменные которые надо найти
+        /// </summary>
+        /// <returns></returns>
+        public List<string> returnQuestionFindParams()
+        {
+            return QuestionFindParams;
         }
     }
 }
