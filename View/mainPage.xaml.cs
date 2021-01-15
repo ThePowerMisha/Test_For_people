4e332c2a74ded1bc16aabc4ca9b44eac68a4ec1b
Alex <69731857+AlexanderTheGod@users.noreply.github.com>
2021-01-16 00:28:14 +0300

    Add files via upload

--- View/mainPage.xaml.cs
+++ View/mainPage.xaml.cs
@@ -46,6 +46,9 @@ namespace WpfApp1.View {
             Placeholder.add(secondName, "Отчество");
             Placeholder.add(group, "Группа");
             //=============mainStyle================
+
+            //if (MainWindow.getScreenWidth()<1680)
+            //    MainWindow.adaptiveText(mainPageTitle.Content.ToString(), mainPageTitle.ActualWidth, mainPageTitle);
         }
         private static resultsPage rPage;
         private static choiceBlock cBlock;
@@ -179,7 +182,7 @@ namespace WpfApp1.View {
 
         //КНОПКА ПОДТВЕРДИТЬ
         private static bool popupButtonFlag = true;
-        private static string passwordKey = "12345";
+        public static string passwordKey = "12345";
         private async void popupButton_Click(object sender, RoutedEventArgs e) {
             if (popupButtonFlag == true) {
                 popupButtonFlag = false;
@@ -237,6 +240,17 @@ namespace WpfApp1.View {
         private void popupButton_PreviewKeyDown(object sender, KeyEventArgs e) {
             if (e.Key == Key.Enter)
                 popupButton_Click(sender, e);
+        }
+
+
+        /// <summary>
+        /// АДАПТИВ МАЗАФАКА
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mainPageTitle_Loaded(object sender, RoutedEventArgs e) {
+            if (MainWindow.getScreenWidth() < 1680)
+                MainWindow.adaptiveText(mainPageTitle.Content.ToString(), mainPageTitle.ActualWidth, mainPageTitle);
         }
         //=======================mainLayout=======================
     }
